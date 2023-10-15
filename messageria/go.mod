638847f01137821f3ba8367a254244cc5bb36067
Rapha <1766429+raphaelrreis@users.noreply.github.com>
2023-10-15 15:22:05 -0300

    initial commit
    messageria with go

---
+++ messageria/go.mod
@@ -0,0 +1,8 @@
+module github/devfullcycle/go-messageria
+
+go 1.21
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+)
---
+++ messageria/usename/product_repository_mysql.go
@@ -0,0 +1,31 @@
+package usecase
+
+import "github/devfullcycle/go-messageria/entity"
+
+type CreateProductInputDto struct {
+	Name  string
+	Price float64
+}
+
+type CreateProductOutputDto struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+type CreateProductUseCase struct {
+	ProductRepository entity.ProductRepository
+}
+
+func (u *CreateProductUseCase) Execute(input CreateProductInputDto) (*CreateProductOutputDto, error) {
+	product := entity.NewProduct(input.Name, input.Price)
+	err := u.ProductRepository.Create(product)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateProductOutputDto{
+		ID:    product.ID,
+		Name:  product.Name,
+		Price: product.Price,
+	}, nil
+}
